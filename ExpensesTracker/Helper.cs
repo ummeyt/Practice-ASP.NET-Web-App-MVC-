f25aa8f9da640cb99bf541c9819353a1bf4a46b2
ummeyt <35542644+ummeyt@users.noreply.github.com>
2021-06-14 08:13:01 -0600

    Add files via upload

---
+++ ExpensesTracker/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Configuration;
+using Microsoft.IdentityModel.Protocols;
+
+namespace ExpensesTracker
+{
+    public static class Helper
+    {
+        public static string CnnVal(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
